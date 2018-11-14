b94c396d439b1c7c290c72882865af90fa1ba69e
Kenny Cuoq <kenny.cuoq@gmail.com>
2018-11-14 17:43:12 +0800

    Create review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_093957) do
+ActiveRecord::Schema.define(version: 2018_11_14_094215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,16 @@ ActiveRecord::Schema.define(version: 2018_11_14_093957) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "meal_id"
+    t.bigint "user_id"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["meal_id"], name: "index_reviews_on_meal_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -58,4 +68,6 @@ ActiveRecord::Schema.define(version: 2018_11_14_093957) do
 
   add_foreign_key "customers", "users"
   add_foreign_key "employees", "users"
+  add_foreign_key "reviews", "meals"
+  add_foreign_key "reviews", "users"
 end
