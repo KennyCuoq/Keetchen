6284bd254d434b74e379eb8316c23e0cd5770f9c
Kenny Cuoq <kenny.cuoq@gmail.com>
2018-11-15 12:36:19 +0800

    First draft at drafting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :meals do
+
+  end
+  resources :meal_dates, except: [ :show ] do
+    resources :orders, only: [ :index, :show, :update ]
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+# new
+# create
+# index
+# show
+# edit
+# patch
+# delete
+
+# can see all meal_dates
+# can show one meal dates? NO
+# meal date new => yes, admin
+# can create a new meal date => yes, admin
+# can edit a meal and update a meal => yes, admin
+# can delete a meal date => yes admin
+
