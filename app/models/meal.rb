ab9fd2972ddb1ba0cb0ac02423a9cf5802c50df5
Kenny <kenny.cuoq@gmail.com>
2018-11-15 12:12:46 +0800

    Update meal.rb
    What do you guys think about that change?

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,8 +2,10 @@ class Meal < ApplicationRecord
   has_many :meal_dates
   has_many :orders, through: :meal_dates
   has_many :users, through: :orders
+  has_many :customers, through: :users
   has_many :reviews
-  has_many :orders, through: :meal_dates
+#   has_many :orders, through: :meal_dates
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
+  validates :photo, presence: true
 end
