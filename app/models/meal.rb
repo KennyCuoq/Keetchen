cd5e141f078b58e3a96e8d5b744fb2e2742aaf2a
Kenny <kenny.cuoq@gmail.com>
2018-11-15 12:15:11 +0800

    Update meal.rb

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,7 +2,6 @@ class Meal < ApplicationRecord
   has_many :meal_dates
   has_many :orders, through: :meal_dates
   has_many :users, through: :orders
-  has_many :customers, through: :users
   has_many :reviews
 #   has_many :orders, through: :meal_dates
   validates :name, presence: true, uniqueness: true
