bc2c2081cb7f255c54b1fd492372c7d4cc1410ed
Kenny <kenny.cuoq@gmail.com>
2018-11-15 12:34:10 +0800

    Update 20181115031130_create_orders.rb

--- db/migrate/20181115031130_create_orders.rb
+++ db/migrate/20181115031130_create_orders.rb
@@ -3,7 +3,7 @@ class CreateOrders < ActiveRecord::Migration[5.2]
     create_table :orders do |t|
       t.references :meal_date, foreign_key: true
       t.references :user, foreign_key: true
-      t.string :status, default: 'Requested'
+      t.string :status, default: 'Pending'
       t.integer :quantity, default: 1
       t.string :qr_code
       t.boolean :pre_order, default: false
