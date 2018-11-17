ea9200470170a548b5b76975570a891eea0fa0da
steven van eecke <stevenvaneecke@hotmail.com>
2018-11-17 15:14:04 +0800

    customerid when signing up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
+
+
 
   resources :customers, except: [ :new, :index ]do
     collection do
