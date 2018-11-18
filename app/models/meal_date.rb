c9bec5af42f296a63a765dbe8c4584d2869e62a9
Kenny Cuoq <kenny.cuoq@gmail.com>
2018-11-18 23:57:28 +0800

    furher tests with uncached

--- app/models/meal_date.rb
+++ app/models/meal_date.rb
@@ -7,10 +7,12 @@ class MealDate < ApplicationRecord
   def qr_code(user)
     # raise
     # Order.where("user_id = ?", current_user)
+    Order.uncached do
     order = user.orders.where("meal_date_id = ?", self)
     if !order.empty?
       qr_string = order[0].qr_code
       RQRCode::QRCode.new(qr_string, size: 4)
+    end
     end
     # order[0].qr_code if !order.empty?
     # order ? order[0].id : 'DOGEEE'
