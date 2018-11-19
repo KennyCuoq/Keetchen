c0e187829a7881717308de208f782f0247332cd6
Andreas Van Assche <andreasava@hotmail.com>
2018-11-19 16:30:00 +0800

    finalization of employee index

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,22 +3,24 @@ class CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
 
-<<<<<<< HEAD
- def show
-  @customer = Customer.find(params[:id])
- end
+  def index
+    @customers = Customer.all
+  end
 
- def index
-  @customers = Customer.all
- end
-=======
   def update
     @customer = Customer.find(params[:id])
     @customer.update(customer_params)
   end
 
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy!
+  end
+
+  private
+
   def customer_params
     params.require(:customer).permit(:photo)
   end
->>>>>>> 85ce00c22c51215586bdef8cf02835ceabcf64f2
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,14 @@ Rails.application.routes.draw do
     # Route to show customer's order history (where they can review)
       get 'my_orders', to: "customers#my_orders"
     end
+    resources :employees, only: [ :create ]
   end
 
   post "confirm_order", to: "orders#confirm"
-  resources :employees, except: [:destroy, :new, :edit]
+  resources :employees, except: [ :new, :edit, :create]
   resources :reviews, only: [ :create ]
-  resources :meals, except: [ :show, :delete ]
-  resources :meal_dates, except: [ :show, :delete ] do
+  resources :meals, except: [ :show, :destroy ]
+  resources :meal_dates, except: [ :show, :destroy ] do
     resources :orders, only: [ :index, :show, :create, :update, :new] do
       resources :payments, only: [:new, :create]
     end
