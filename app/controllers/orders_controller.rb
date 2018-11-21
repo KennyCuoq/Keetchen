73331cc4526ac1845b5991fec482884738362b07
Kenny Cuoq <kenny.cuoq@gmail.com>
2018-11-21 12:27:15 +0800

    test without csrf

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :determine_variables, only: [:new, :create]
-  skip_before_filter :verify_authenticity_token, only: [:confirm]
+  skip_before_action :verify_authenticity_token, only: [:confirm]
   skip_before_action :authenticate_user, only: [:confirm]
 
    def index
