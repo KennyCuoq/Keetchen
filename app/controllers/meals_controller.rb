6059a533d0bba513bee965c16fcbb01e433f9f77
steven van eecke <stevenvaneecke@hotmail.com>
2018-11-22 10:15:18 +0800

    removed login in navbar, was bugging

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,20 @@
+class MealsController < ApplicationController
+  def new
+    @meal = Meal.new()
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+      if @meal.save
+      redirect_to meals_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:name, :description, :photo)
+  end
+end
