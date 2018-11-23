484dbd38280ffe7abf5d5436960dfa88b555a041
Kenny Cuoq <kenny.cuoq@gmail.com>
2018-11-23 11:57:57 +0800

    fixin https in prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
    config.action_cable.url = 'wss://www.keetchen.co/cable'
-   config.action_cable.allowed_request_origins = [ 'http://www.keetchen.co', 'http://www.keetchen.co', 'http://keetchen.co', 'https://keetchen.co' ]
+   config.action_cable.allowed_request_origins = [ 'http://www.keetchen.co', 'https://www.keetchen.co', 'http://keetchen.co', 'https://keetchen.co' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
