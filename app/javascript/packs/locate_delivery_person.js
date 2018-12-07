3a6cd6f22fe5ef2db805ab5484a8ae8ba7e2e61a
Kenny Cuoq <kenny.cuoq@gmail.com>
2018-12-07 18:57:00 +0800

    employees can now choose when to start sharing lcoation and when to stop

--- app/javascript/packs/locate_delivery_person.js
+++ app/javascript/packs/locate_delivery_person.js
@@ -47,10 +47,31 @@ function geoFindMe(employeeID) {
 
   output.innerHTML = "<p>Locating…</p>";
 
-  navigator.geolocation.watchPosition(success, error);
+  return navigator.geolocation.watchPosition(success, error);
+  // const watchId = navigator.geolocation.watchPosition(success, error);
+  // console.log(watchId);
   // navigator.geolocation.getCurrentPosition(success, error);
 }
 
+function stopGeoLocation(employeeID) {
+  const GPSCoordinates = {
+    lat: 'no lat',
+    lng: 'no lng',
+  }
+
+  fetch(`/employees/${employeeID}/update_position`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      'X-CSRF-Token': document.querySelector("[name='csrf-token']").content,
+      "Content-Type": 'application/json',
+      "Accept": 'application/json'
+    },
+    body: JSON.stringify(GPSCoordinates),
+    credentials: 'same-origin'
+  })
+}
+global.stopGeoLocation = stopGeoLocation;
 global.geoFindMe = geoFindMe;
 
 // create a button tha triggers geofindme
